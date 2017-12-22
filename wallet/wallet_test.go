0e386c44218eeccf636ba090d819b26768ce6d98
pocockn <pocockn@hotmail.co.uk>
2017-12-22 15:50:32 +0000

    test that wallet has correct balance after we initialise it

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -11,3 +11,14 @@ func TestWalletCreation(t *testing.T) {
 		t.Error("Unexpected value, should be ", expectedUnits)
 	}
 }
+
+func TestWalletHasCorrectBalance(t *testing.T) {
+	coinMap := make(map[string]int)
+	expectedUnits := 100
+	coinMap["BTC"] = 100
+	btcWallet := NewWallet(coinMap)
+	actualUnits := btcWallet.SpecificBalance("BTC")
+	if expectedUnits != actualUnits {
+		t.Error("Unexpected value, should be ", expectedUnits)
+	}
+}

a17d1434068d9fad3c1c72b20d1f0d85da62b6bd
pocockn <pocockn@hotmail.co.uk>
2017-12-28 14:18:49 +0000

    Use the new global DB var when creating the DB schema in the main func

--- main.go
+++ main.go
@@ -1,28 +1,76 @@
 package main
 
-// echo similar to Ratpack, lightweight HTTP program
-
-/*
-* To import a package solely for its side-effects (initialization),
-* the underscore import is used for the side-effect of
-* registering the sqlite3 driver as a database driver
-* in the init() function, without importing any other functions:
- */
-
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
+	"github.com/go-pg/pg/orm"
+
 	"github.com/pocockn/crypto-compare-go/handlers"
+	"github.com/pocockn/crypto-compare-go/models"
 
 	"github.com/pocockn/crypto-compare-go/api"
 	"github.com/pocockn/crypto-compare-go/wallet"
 
+	"math/rand"
+
+	"github.com/go-pg/pg"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	_ "github.com/mattn/go-sqlite3"
 )
+
+func main() {
+	models.InitDB()
+	err := createSchema(models.DB)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a new instance of Echo
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// CORS restricted
+	// allow request from any localhost address
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:5000"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
+
+	// Fetchs a list of coins from the cryptocompare API
+	e.GET("/", func(context echo.Context) error {
+		return context.JSON(http.StatusOK, api.FetchCoinList())
+	})
+
+	e.GET("/html", func(context echo.Context) error {
+		return context.File("public/index.html")
+	})
+
+	e.GET("/createWallet", createWallet)
+
+	e.POST("/deposit", depositFunds)
+
+	e.POST("/withdraw", withdrawFunds)
+
+	e.GET("/wallet", func(context echo.Context) error {
+		var wallets []wallet.Wallet
+		err = models.DB.Model(&wallets).Select()
+		if err != nil {
+			panic(err)
+		}
+		return context.JSON(200, wallets)
+	})
+
+	e.GET("/coin", handlers.GetCoin)
+
+	// Start the web server
+	e.Start(":8000")
+
+}
 
 // CreateWallet initialises a user wallet based on one coin and some initial units
 func createWallet(c echo.Context) error {
@@ -34,6 +82,19 @@ func createWallet(c echo.Context) error {
 	}
 	coinMap[coin] = units
 	btcWallet := wallet.NewWallet(coinMap)
+	db := pg.Connect(&pg.Options{
+		Database: "crypto_compare",
+		User:     "pocockn",
+		Password: "only8deb",
+	})
+	wallet := &wallet.Wallet{
+		ID:        rand.Int(),
+		CoinsHeld: coinMap,
+	}
+	errDb := db.Insert(wallet)
+	if errDb != nil {
+		panic(errDb)
+	}
 	return c.JSON(http.StatusCreated, btcWallet)
 }
 
@@ -69,57 +130,13 @@ func withdrawFunds(c echo.Context) error {
 	return c.JSON(http.StatusOK, btcWallet)
 }
 
-func main() {
-
-	// Create a new instance of Echo
-	// e := is short hand for var e =
-	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// CORS default
-	// Allows requests from any origin with GET,HEAD,PUT,POST,DELETE
-	// e.Use(middleware.CORS())
-
-	// CORS restricted
-	// allow request from any localhost address
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:5000"},
-		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
-	}))
-
-	// Fetchs a list of coins from the cryptocompare API
-	e.GET("/", func(context echo.Context) error {
-		return context.JSON(http.StatusOK, api.FetchCoinList())
+func createSchema(db *pg.DB) error {
+	err := db.CreateTable(&wallet.Wallet{}, &orm.CreateTableOptions{
+		IfNotExists: true,
 	})
-
-	e.GET("/html", func(context echo.Context) error {
-		return context.File("public/index.html")
-	})
-
-	e.GET("/createWallet", createWallet)
-
-	e.POST("/deposit", depositFunds)
-
-	e.POST("/withdraw", withdrawFunds)
-
-	e.GET("/wallet", func(context echo.Context) error {
-		return context.JSON(http.StatusOK, nil)
-	})
-
-	e.GET("/coin", handlers.GetCoin)
-
-	// Similar to Ratpack handler, route takes a pattern and then
-	// a handler function as param
-	// e.GET("/tasks", handlers.GetTasks(db))
-
-	// // In GOLang the type comes after variable
-	// // eg context is of type echo.Context
-	// e.PUT("/tasks", handlers.PutTasks(db))
-
-	// e.DELETE("/tasks/:id", handlers.DeleteTask(db))
-
-	// Start the web server
-	e.Start(":8000")
-
+	if err != nil {
+		return err
+	}
+	log.Println("Database schema created")
+	return nil
 }
